1b34aaddec6da3d3cf02deb6ba0243a92bf0133f
ByeolAhn <bahn1@my.centennialcollege.ca>
2023-11-18 10:13:45 -0500

    home and no page add, index.js and .css edit

--- usedbookstore/src/index.js
+++ usedbookstore/src/index.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './components/header';
+import Home from './pages/home';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import '@fortawesome/fontawesome-free/js/all.min.js'
 
 import reportWebVitals from './reportWebVitals';
 
-export default function App(){
+export default function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path = "/" elemennt = {<Header />}>
-
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path="/" elemennt={<Header />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
