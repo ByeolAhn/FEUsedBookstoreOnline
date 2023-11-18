315d3e95565e89de008b2bfa57ec561392f2ed8c
ByeolAhn <bahn1@my.centennialcollege.ca>
2023-11-18 09:50:15 -0500

    logo add, header edit1, index edit1

--- usedbookstore/src/index.js
+++ usedbookstore/src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Routes} from "react-router-dom";
+import Header from './components/books/header';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+export default function App(){
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Routes path = "/" elemennt = {<Header />}>
+
+      </Routes>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
