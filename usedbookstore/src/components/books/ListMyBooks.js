45a3981f385bc6798d9751c440040b7fbe0db9d2
ByeolAhn <bahn1@my.centennialcollege.ca>
2023-12-10 15:36:49 -0500

    empty list error correct

--- usedbookstore/src/components/books/ListMyBooks.js
+++ usedbookstore/src/components/books/ListMyBooks.js
@@ -50,7 +50,7 @@ const ListMyBooks = () => {
                 <div className="table-responsive">
                     {isLoading ? (
                         <p>Loading...</p>
-                    ) : myBooks.length === 0 ? (
+                    ) : (!myBooks || !myBooks.books || myBooks.books.length === 0) ? (
                         <p>Your List is empty</p>
                     ) : (
                         <table className="table table-bordered table-striped table-hover">
@@ -77,7 +77,7 @@ const ListMyBooks = () => {
                                         <td style={hasExpired(book.expiryDate) ? { color: 'red' } : {}}>{book.condition}</td>
                                         <td style={hasExpired(book.expiryDate) ? { color: 'red' } : {}}>{book.price}</td>
                                         <td style={hasExpired(book.expiryDate) ? { color: 'red' } : {}}>{book.description}</td>
-                                        <td style={hasExpired(book.expiryDate) ? { color: 'red' } : {}}>{hasExpired(book.expiryDate) ? "Expired" : (product.active ? "Available" : "Unavailable")}</td>
+                                        <td style={hasExpired(book.expiryDate) ? { color: 'red' } : {}}>{hasExpired(book.expiryDate) ? "Expired" : (book.active ? "Available" : "Unavailable")}</td>
                                         <td>
                                             <Link
                                                 to={`/books/update/${book.isbn}`}
